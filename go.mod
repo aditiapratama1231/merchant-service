260d6a5ce6f9460873f6d565cb6c10c0a238d65f
I Gede Wicaksana <wicaksanaigede@gmail.com>
2019-07-05 14:55:00 +0700

    tried call inventory service

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/go-kit/kit v0.9.0
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
+	github.com/golang/protobuf v1.2.0
 	github.com/gorilla/mux v1.7.3
 	github.com/kujtimiihoxha/kit v0.1.1 // indirect
 	google.golang.org/grpc v1.22.0
--- pkg/service/MerchantService.go
+++ pkg/service/MerchantService.go
@@ -2,6 +2,11 @@ package service
 
 import (
 	"context"
+	"log"
+	pbproduct "qasir-supplier/merchant/pb/product"
+	"time"
+
+	"google.golang.org/grpc"
 )
 
 type MerchantService interface {
@@ -14,7 +19,26 @@ func NewMerchantService() MerchantService {
 	return merchantService{}
 }
 
+// GetProduct from Inventory Service
+func GetProduct(client pbproduct.ProductsClient, point *pbproduct.ProductRequest) string {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	feature, err := client.GetProducts(ctx, point)
+	if err != nil {
+		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	return feature.Message
+}
+
 // Get will return today's date
 func (merchantService) GetMerchants(ctx context.Context) (string, error) {
-	return "Merchants Successfully Loaded", nil
+	conn, err := grpc.Dial("localhost:8081", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	// client := pb.NewAddClient(conn)
+	client := pbproduct.NewProductsClient(conn)
+	product := GetProduct(client, &pbproduct.ProductRequest{Id: 1})
+	return "Merchants Successfully Loaded" + " and " + product, nil
 }
